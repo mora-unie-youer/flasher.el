cc29f821950baa053335df224d90577259ea3202
Mora Unie Youer <mora_unie_youer@riseup.net>
2022-03-24 16:36:59 +0000

    Created flasher.el

---
+++ flasher.el
@@ -0,0 +1,40 @@
+;;; flasher.el --- Simple and extensible flashcard system  -*- lexical-binding: t; -*-
+;;
+;; Filename: flasher.el
+;; Description: Simple and extensible flashcard system
+;; Author: Mora Unie Youer <mora_unie_youer@riseup.net>
+;; Maintainer: Mora Unie Youer <mora_unie_youer@riseup.net>
+;; Copyright (c) 2022 Mora Unie Youer
+;; Created: Mar 24 2022
+;; URL: https://github.com/mora-unie-youer/flasher
+;;      https://gitlab.com/mora-unie-youer/flasher
+;;      https://notabug.org/mora-unie-youer/flasher
+;;      https://codeberg.org/mora-unie-youer/flasher
+
+;;; Commentary:
+;;
+;; Simple and extensible flashcard system
+
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be included in all
+;; copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
+;;; Code:
+
+(provide 'flasher)
+
+;;; flasher.el ends here
