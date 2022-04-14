92bbebaf94f40d15fad6b0974fcaaf51722a542e
Mora Unie Youer <mora_unie_youer@riseup.net>
2022-04-14 12:44:00 +0000

    Changed 'normal card variant name

--- flasher-type-normal.el
+++ flasher-type-normal.el
@@ -49,7 +49,7 @@
 
 (defun flasher-type-normal-var-init (&optional id)
   "Initialize 'normal card variants for card with ID."
-  (flasher-card--update-variants '("front") id))
+  (flasher-card--update-variants '("back") id))
 
 (defun flasher-type-normal-setup (_variant)
   "Prepare a 'normal card for review."
